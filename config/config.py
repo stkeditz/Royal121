350c3c7a8cb1827df4e5679cee3406e097522827
saste bots <120207896+aina7877@users.noreply.github.com>
2023-02-01 21:14:02 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,14 +7,14 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "16832093"))
-API_HASH = getenv("API_HASH", "c27c9eb522e6c85dd02e542fd7606b4f")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001715514181"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "aina music bot")
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5140509190").split()))
 
