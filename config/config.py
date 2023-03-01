cd0eda404e28945b915c607b65b82865127c5278
SAGAR1215g <126651350+SAGAR1215g@users.noreply.github.com>
2023-03-01 13:01:32 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -12,11 +12,11 @@ API_HASH = getenv("API_HASH", "")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Dilmusic:stkeditz121@cluster0.3juq0j6.mongodb.net/?retryWrites=true&w=majority")
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001828908632"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5140509190").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5740391440").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
