eef46c4e815e05e8bb342d9e3b5e7defaab72f6e
SAGAR1215g <126651350+SAGAR1215g@users.noreply.github.com>
2023-03-13 12:06:34 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "6117249994").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/SWAG_BOTS")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/DEADLY_NETWORK")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MR_YAMRAJ_XD")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/SWAG_HOUSE_CHAT")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DEADLY_OFFIC1AL")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
