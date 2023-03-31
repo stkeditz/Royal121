12f0f3fa12a7e6ad4494885a601c39227442e439
SAGAR1215g <126651350+SAGAR1215g@users.noreply.github.com>
2023-03-31 11:17:30 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5822966959").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/Sanki_Network")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "@Sanki_Network")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MR_YAMRAJ_XD")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Drishti_Education_Hub")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "@MoonlightChatting")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
