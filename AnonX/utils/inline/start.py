240e0be4adc73d24c608ec1ef98305ef7f7e08d3
SAGAR1215g <126651350+SAGAR1215g@users.noreply.github.com>
2023-04-07 15:13:30 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -30,13 +30,8 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=config.SUPPORT_GROUP
             ),
         [
-            InlineKeyboardButton(
-                text="🥀 ғᴇᴇʟɪɴɢs 🥀",
-                url=f"https://t.me/YaMraJ_FeeLinG"
-            ),
-            InlineKeyboardButton(
-                text="✨ʙᴏᴛ sᴜᴩᴩᴏʀᴛ ✨", 
-                url=f"https://t.me/SankiDiscuss"
+           InlineKeyboardButton(
+                text="🥀 Feelings 🥀", url=f"https://t.me/Yamraj_Feeling",
             ),
         ],
      ]
@@ -66,16 +61,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             InlineKeyboardButton(
                     text="❄️ ᴄʜᴀɴɴᴇʟ ❄️", url=config.UPSTREAM_REPO
                 )
-        ],
-        [
-            InlineKeyboardButton(
-                text="🥀 ғᴇᴇʟɪɴɢs 🥀",
-                url=f"https://t.me/YaMraJ_FeeLinG"
-            ),
-            InlineKeyboardButton(
-                text="✨ʙᴏᴛ sᴜᴩᴩᴏʀᴛ ✨", 
-                url=f"https://t.me/SankiDiscuss"
-            ),
         ],
      ]
     return buttons
