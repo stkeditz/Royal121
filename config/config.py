6894429bb7837d05d87c0e33f39ddee0f718ee7b
bnznsnsnsksk <126651350+SAGAR1215g@users.noreply.github.com>
2023-04-21 23:52:23 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "6006313871").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/SAGAR1215g/YAMRAJXMUSIC")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/Sanki_Server")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
